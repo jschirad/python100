a2b23c9f12a2ffb1d349e4a8b2bfd816cb2faeb7
Facu Schirado <facuschirado@Facus-MacBook-Air.local>
2021-12-14 20:57:27 +0100

    First day

---
+++ day00/challenge.py
@@ -0,0 +1,16 @@
+# First Challenge
+print("Hello!")
+print("First Challenge")
+myName = input("What is your name? ")
+print("It is good to meet you, "+myName)
+print("The length of your name is: "+str(len(myName)))
+
+# Second Challenge
+print("Second Challenge")
+a = input("Enter a number: a =")
+b = input("Enter another number: b =")
+
+# swap values
+a, b = b, a
+print("a is now: "+a)
+print("b is now: "+b)
---
+++ day00/main.py
@@ -0,0 +1,9 @@
+# My first codeline in python
+print("Hello World!")
+print("Hello"+" "+"Facu")
+# Input () will get user input in console
+print("What is your name?")
+myName = input()
+print("It is good to meet you, "+myName)
+
+
