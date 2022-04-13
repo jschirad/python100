3ff21dadbf9da232d9a1306edbcc69cc885c308d
Facundo  Schirado <jfacuscirado@gmail.com>
2022-04-13 16:01:46 +0200

    day 09

--- day09-Dictonary/dictionary04.py
+++ day09-Dictonary/dictionary04.py
@@ -2,12 +2,15 @@ import os
 import art
 #HINT: You can call clear() to clear the output in the console.
 clear = lambda: os.system('clear')
+# Empty dictionary
 dictionary = {}
+
+# Add new value to dictionary
 def add_new_value(name, bid):
     dictionary[name] = bid
     return dictionary
 
-
+# Ask user for name and bid
 def main():
     clear()
     print(art.logo)
@@ -15,15 +18,20 @@ def main():
     while switch != False:
         name = input("What is your name? ")
         bid = input("What is your bid? $")
+        # Add new value to dictionary
         add_new_value(name, bid)
+        # Ask user if they want to continue
         another = input("There is another bid? (y/n) ")
         if another == "n":
             switch = False
         else:
             clear()
+
+# Calculate highest bid
 def high_score():
     clear()
     max_value = 0
+    # Loop through dictionary
     for key, value in dictionary.items():
         if int(value) > int(max_value):
             max_value = value
@@ -31,7 +39,9 @@ def high_score():
     print("The highest bid is from", max_key, "with a bid of $", max_value)
     print(art.logo)
 
+# Call main function
 main()
+# Call high_score function
 high_score()
 
 
